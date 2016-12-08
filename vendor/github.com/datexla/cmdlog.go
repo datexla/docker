5d0de4b3273e75a21a2f032c76607e2996058966
Wsine <weizhy3@hotmail.com>
2016-12-08 21:14:59 +0800

    update datexla cmdlog lib

--- vendor/github.com/datexla/cmdlog.go
+++ vendor/github.com/datexla/cmdlog.go
@@ -5,9 +5,17 @@ import (
 	"os/exec"
 )
 
-func write(content string, pathToFile string) {
+var (
+	defaultPathToFile = "/var/log/docker/scheduler.log"
+	serviceCome       = "[service come  ]"
+	serviceFinish     = "[service finish]"
+	scorePrint        = "[score   print ]"
+)
+
+func write(state string, event string, pathToFile string) {
 	t := time.Now().Format(time.UnixDate)
-	command := "echo \"" + t + " " + content + "\" >> " + pathToFile
+	content := t + "\t" + state + "\t" + event
+	command := "echo \"" + content + "\" >> " + pathToFile
 	cmd := exec.Command("/bin/sh", "-c", command)
 	cmd.Run()
 }
